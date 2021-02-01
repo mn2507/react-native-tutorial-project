a35131a883a158a3041930130444ea5ba9b2aad2
Muthu Narayanan <muthu.narayanan.hexainitiative.com>
2021-02-02 00:29:12 +0800

    Showing text input using state

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -29,6 +29,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Square")}
         title="Go to Square Page"
       />
+       <Button
+        onPress={() => navigation.navigate("Text")}
+        title="Go to Text Page"
+      />
     </View>
   );
 };
